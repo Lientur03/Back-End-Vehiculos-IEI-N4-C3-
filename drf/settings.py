47f4036bb0f64378d8b8ff04aab911cc4c447fb7
Lientur03 <andreslientur03@gmail.com>
2026-09-03 10:39:24 -0400

    Implementación y Verificación de la Página De Error 404.html [2 horas de trabajo autónomo] | A.M. [8:46 - 10:46]

--- drf/settings.py
+++ drf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9bpk#dt6s970#gi6(_o$3tx4k01tat*2n0a(s_0&&k4g%w8lc)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 
 # Application definition
