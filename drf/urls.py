aaed70e61bf07ca3003f7228c5f370259e6f09c3
Lientur03 <andreslientur03@gmail.com>
2026-09-03 20:22:32 -0400

    Terminado el Trabajo Autónomo [20:22] 03-09-2026

--- drf/urls.py
+++ drf/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vehiculos.urls')),
+    path('', include('vehiculos.urls')), # O la vista de tu página de inicio
 ]
+
+# Servir estáticos en entorno local independientemente del estado de DEBUG
+urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'vehiculos' / 'static')
